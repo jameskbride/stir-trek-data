66015d9156f2723cd7ca70be59b24cb88b149624
Jim Kirkbride <james.kirkbride@gmail.com>
2019-03-03 16:21:45 -0500

    Initial commit.

---
+++ gridtable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -X GET \
+  'http://localhost:8181/api/v2/m4qufajl/view/gridtable' \
+  -H 'cache-control: no-cache'
---
+++ speakers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -X GET \
+  'http://localhost:8181/api/v2/m4qufajl/view/speakers' \
+  -H 'cache-control: no-cache'
